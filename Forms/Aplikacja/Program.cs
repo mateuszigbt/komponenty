88ac7bc9d2fbab83cc4a3f14908ed060ef900c4c
PawelStaniaszek <pawel.staniaszek1@gmail.com>
2022-05-15 16:32:40 +0200

    projekt

--- Forms/Aplikacja/Program.cs
+++ Forms/Aplikacja/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 ArduinoOdbieranieDanych.OdbieranieDanych odbior = new ArduinoOdbieranieDanych.OdbieranieDanych();
-while (true) { 
-    Console.WriteLine(odbior.temperatura);
+while (true) {
+    foreach (var item in odbior.wartosci)
+    {
+        Console.WriteLine(item);
+    }
+    
     Task.Delay(1000).Wait();
 }
 
